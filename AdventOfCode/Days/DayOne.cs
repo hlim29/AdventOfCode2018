23b428eb3a051fd4c202a9abac676d98218a39d4
Henry Lim <hlim29@users.noreply.github.com>
2018-12-04 21:54:49 +1100

    Day one

---
+++ AdventOfCode/Days/DayOne.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Days
+{
+    public class DayOne
+    {
+        public void Calibrate()
+        {
+            var fileContents = ReadFile();
+            var parsedContents = ParseFile(fileContents);
+            Console.WriteLine($"The sum is: {parsedContents.Sum()}");
+        }
+
+        public void FirstFrequency()
+        {
+            var fileContents = ReadFile();
+            var parsedContents = ParseFile(fileContents);
+
+            var frequencies = new List<int>();
+
+            var currentFrequency = 0;
+
+            while (true)
+            {
+                foreach (var offset in parsedContents)
+                {
+                    if (frequencies.Contains(currentFrequency))
+                    {
+                        Console.WriteLine($"The first duplicate is: {currentFrequency}");
+                        return;
+                    }
+
+                    frequencies.Add(currentFrequency);
+                    currentFrequency += offset;
+                }
+            }
+        }
+
+        public string ReadFile()
+        {
+            return File.ReadAllText(@"Inputs/dayOne.txt");
+        }
+
+        public List<int> ParseFile(string fileContents)
+        {
+            var delimitedFile = fileContents.Split("\n");
+            var result = new List<int>();
+
+            foreach(var line in delimitedFile)
+            {
+                if (int.TryParse(line, out var parsedLine))
+                {
+                    result.Add(parsedLine);
+                }
+            }
+            return result;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode.Days;
+using System;
 
 namespace AdventOfCode
 {
@@ -7,9 +8,34 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Advent of Code 2018");
-            Console.WriteLine("Please enter in a day (1-25):");
-            var input = ParseInput(Console.ReadLine());
+            var input = -1;
+            while (input == -1)
+            {
+                input = AskForDay();
+                switch (input)
+                {
+                    case 1:
+                        var calibrator = new DayOne();
+                        calibrator.Calibrate();
+                        calibrator.FirstFrequency();
+                        break;
+                    case -1:
+                        Console.WriteLine("Invalid day. Please try again.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid day. Please try again.");
+                        break;
+                }
+                input = -1;
+            }
+            
             //TODO: Days go here
+        }
+
+        static int AskForDay()
+        {
+            Console.WriteLine("Please enter in a day (1-25):");
+            return ParseInput(Console.ReadLine());
         }
 
         static int ParseInput(string input)
