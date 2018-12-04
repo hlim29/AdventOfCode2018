e33729e8cd8f929918ce200d1a72f45fa3f05d6d
Henry Lim <hlim29@users.noreply.github.com>
2018-12-04 20:54:19 +1100

    Base project

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Advent of Code 2018");
+            Console.WriteLine("Please enter in a day (1-25):");
+            var input = ParseInput(Console.ReadLine());
+            //TODO: Days go here
+        }
+
+        static int ParseInput(string input)
+        {
+            var isValid = int.TryParse(input, out var number);
+            if (!isValid)
+                return -1;
+            return number;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+﻿ # Advent of Code
+
+ 'Tis the season for coding (and refining my C# skills). Pull requests are welcome!
+
+ ## About
+ [Advent of Code](https://adventofcode.com) is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. People use them as a speed contest, interview prep, company training, university coursework, practice problems, or to challenge each other.
+
+ ## Days
+
+ Day|Part 1|Part 2
+ -|-|-|
