3d4ccf0476497fae2b7fa3a1e53f6ce77d6003b7
Henry Lim <hlim29@users.noreply.github.com>
2018-12-09 19:25:29 +1100

    Day nine - cleanup

--- AdventOfCode/Days/DayNine.cs
+++ AdventOfCode/Days/DayNine.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode.Days
     public class DayNine
     {
         private readonly int _playerCount;
-        private List<Player> _players;
+        private long[] _players;
         private int _lastMarbleValue;
         private LinkedList<int> _circle;
         private LinkedListNode<int> _currentNode;
@@ -20,17 +20,13 @@ namespace AdventOfCode.Days
             _lastMarbleValue = int.Parse(input[6]);
             _circle = new LinkedList<int>();
             _circle.AddFirst(0);
-            _players = new List<Player>(_playerCount);
-            for (var i = 0; i < _playerCount; i++)
-            {
-                _players.Add( new Player { Score = 0 });
-            }
+            _players = new long[_playerCount];
         }
 
         public void GetHiscore()
         {
             Play();
-            Console.WriteLine($"The hiscore is {_players.Max(x => x.Score)}");
+            Console.WriteLine($"The hiscore is {_players.Max()}");
         }
 
         public void GetHiscoreWithHighMarble()
@@ -44,15 +40,15 @@ namespace AdventOfCode.Days
             _currentNode = _circle.First;
             for (int i = 1; i <= _lastMarbleValue; i++)
             {
-                var currentPlayer = _players[i % _playerCount];
+                var currentPlayerIndex = (i % _playerCount);
                 var playerIndex = i % _playerCount;
                 if (i % 23 == 0)
                 {
                     _currentNode = GetPreviousDistance(_currentNode, 6);
-                    currentPlayer.Score += _currentNode.Value;
+                    _players[currentPlayerIndex] += _currentNode.Value;
                     var temp = _currentNode.Next;
                     _circle.Remove(_currentNode);
-                    currentPlayer.Score += i;
+                    _players[currentPlayerIndex] += i;
                     _currentNode = GetPrevious(temp);
                 }
                 else
@@ -85,30 +81,9 @@ namespace AdventOfCode.Days
 
         private LinkedListNode<int> GetNext(LinkedListNode<int> node) => node.Next ?? _circle.First;
 
-        private LinkedListNode<int> SeekTo(LinkedListNode<int> node, int index)
-        {
-            for (int i = 0; i < index; i++)
-            {
-                if (node.Next == null)
-                {
-                    node = _circle.First;
-                }
-                else
-                {
-                    node = node.Next;
-                }
-            }
-            return node;
-        }
-
         private List<string> ReadFile()
         {
             return File.ReadAllText(@"Inputs/daynine.txt").Split(" ").Where(x => !string.IsNullOrEmpty(x)).ToList();
         }
-    }
-
-    public class Player
-    {
-        public long Score { get; set; }
     }
 }
